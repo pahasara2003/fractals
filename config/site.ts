9d460f9a95a4ad8b5cf5af69e045c6b5109fe3c7
pahasara wickramasinghe <138836186+pahasara2003@users.noreply.github.com>
2025-01-27 17:24:59 +0530

    julia added2

--- config/site.ts
+++ config/site.ts
@@ -3,28 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Fractals",
   description: "Mathematical Fractals",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
+  
   navMenuItems: [
     {
       label: "Profile",
