3f7e1a04ffee88adc39f32a441428bdec7219d1c
Pahasara Wickramasinghe <sandeepahasara@gmail.com>
2025-05-09 21:52:23 +0530

    change

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     output: 'export', // Enables static export mode
     images: { unoptimized: true }, // if you use next/image
     trailingSlash: true, // GitHub Pages prefers this
-    basePath: "/<REPO_NAME>", // Replace <REPO_NAME> with your repo name
   };
   
   module.exports = nextConfig;
