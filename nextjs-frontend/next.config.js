f12e3a479dac775975875ddfa64527b12bf10417
Pahasara Wickramasinghe <sandeepahasara@gmail.com>
2025-05-09 20:00:39 +0530

    deploy

--- nextjs-frontend/next.config.js
+++ nextjs-frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-};
-
-module.exports = nextConfig;
+    output: 'export', // Enables static export mode
+    images: { unoptimized: true }, // if you use next/image
+    trailingSlash: true, // GitHub Pages prefers this
+    basePath: "/<REPO_NAME>", // Replace <REPO_NAME> with your repo name
+  };
+  
+  module.exports = nextConfig;
+  
